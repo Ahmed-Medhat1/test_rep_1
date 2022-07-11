e025741f22affc6072f231701f753ddeff9d4218
AhmedMedhat <ahmedmedhat1561@gmail.com>
2022-07-11 19:45:07 +0200

    first commit

---
+++ 0001-DIO_Basics/main.c
@@ -0,0 +1,31 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 11, 2022
+ *      Author: lEGION
+ */
+#include <avr/io.h>
+#include<util/delay.h>
+
+int main(void)
+{
+	DDRA =0x0f ; //0b00001111;
+
+
+	while(1)
+	{
+		//PORTA = 0b00000001;
+	//_delay_ms(1000);
+	//PORTA = 0b00000000;
+	//_delay_ms(1000);
+		PORTA=0b00000001;
+    for(int i=0;i<4;i++)
+    {
+
+    	_delay_ms(250);
+    	PORTA <<= 1;
+
+    }
+	}
+
+}
