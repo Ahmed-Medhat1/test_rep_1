c371ada083e037d56ee40a104da39619bd8212c2
AhmedMedhat <ahmedmedhat1561@gmail.com>
2022-07-11 20:36:19 +0200

    change PORT to PORTB

--- 0001-DIO_Basics/main.c
+++ 0001-DIO_Basics/main.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	DDRA =0x0f ; //0b00001111;
+	DDRB =0x0f ; //0b00001111;
 
 
 	while(1)
@@ -18,12 +18,12 @@ int main(void)
 	//_delay_ms(1000);
 	//PORTA = 0b00000000;
 	//_delay_ms(1000);
-		PORTA=0b00000001;
+		PORTB=0b00000001;
     for(int i=0;i<4;i++)
     {
 
     	_delay_ms(250);
-    	PORTA <<= 1;
+    	PORTB <<= 1;
 
     }
 	}
